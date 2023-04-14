04349fcd9c9024f946050fb6364bc2f4802d1f03
Tony Jimenez <84590027+gertony@users.noreply.github.com>
2023-04-13 23:48:24 -0500

    Update gestor.cpp

--- gestor.cpp
+++ gestor.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main() {
     // Creamos las tareas y las almacenamos
     CrearTarea tarea1("Historia", 12, 4);
-    CrearTarea tarea2("Ciudadania", 14, 4);
-    CrearTarea tarea3("Matematica", 12, 4);
+    CrearTarea tarea2("Sexo", 14, 4);
+    CrearTarea tarea3("Mate", 12, 4);
 
     AlmacenTareas almacen;
     almacen.agregarTarea(tarea1.crearTarea());
     almacen.agregarTarea(tarea2.crearTarea());
     almacen.agregarTarea(tarea3.crearTarea());
-
     // Mostramos las tareas almacenadas
     almacen.mostrarTareas();
 
+    cout<<"\nTiempo restante:"<<endl;
     // Creamos tareas de seguimiento a partir de las tareas almacenadas
     Tarea* tarea = almacen.getTarea(0);
     Seguimiento tareaSeguimiento1(tarea->getCurso(), tarea->getDia(), tarea->getMes(), 12, 4);
@@ -30,5 +30,14 @@ int main() {
     tareaSeguimiento2.mostrar();
     tareaSeguimiento3.mostrar();
 
+    CrearProyecto proyecto1("Robot que hace delivery");
+    AgregarComentario comentario1("Es un robot que hace delivery, primero con una aplicacion eliges el producto que quieres y el robot te lo lleva");
+    
+    AlmacenarProyectos almacenProyec;
+    almacenProyec.agregarProyecto(proyecto1.crearProyecto());
+    AlmacenarComentarios almacenComen;
+    almacenComen.agregarComentario(comentario1.agregarComentario());
+    almacenProyec.mostrarProyectos();
+    almacenComen.mostrarComentarios();
     return 0;
 }
